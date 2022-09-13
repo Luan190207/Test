ad695e82acbf74312ba48335cc082d94913475ae
Luan190207 <113418915+Luan190207@users.noreply.github.com>
2022-09-13 09:19:59 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
 basic.forever(function () {
-	
+    basic.showIcon(IconNames.SmallHeart)
+    music.rest(music.beat(BeatFraction.Whole))
+    basic.showIcon(IconNames.Heart)
+    music.rest(music.beat(BeatFraction.Whole))
 })
